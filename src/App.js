28e46bf2a67c4fd13f349ab55ba30ed9a35511aa
Eokji coding <byunguk98@naver.com>
2023-12-20 00:01:01 +0900

    no message

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {useState, useEffect} from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setTodos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My Beautiful To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+      <input 
+        onChange={onChange} 
+        value={toDo} 
+        type="text" 
+        placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
+     </div>
+  )
+}
+
+export default App;
