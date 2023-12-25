dd738cfa6643afa3ca8190843f939e4e13c4083c
Eokji coding <byunguk98@naver.com>
2023-12-25 19:04:07 +0900

    코인-달러 변환기
    달러를 입력하면 해당 달러에 대한 교환 가능한 코인 수 출력

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import {useState, useEffect} from "react";
 
 function App() {
+  const [selectCoins, setSelectCoins] = useState("0");
+  const [result, setResult] = useState("0");
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+
+const onChange = (event) => {
+  setSelectCoins(event.target.value);
+}
+
+const onSubmit = (event) => {
+  event.preventDefault();
+  const priceUSD = event.target[0].value;
+  calculate(priceUSD);
+}
+
+const calculate = (priceUSD) => {
+  setResult(priceUSD / selectCoins);
+}
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers").then(response => 
     response.json()
@@ -13,10 +30,18 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>The Coins ({coins.length})</h1>
-      {loading ? (<strong>Loading...</strong>) : <ul>
-        {coins.map((coin) => <li>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</li>)}
-      </ul>}
+      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (<strong>Loading...</strong>) : 
+      <select onChange={onChange}>{coins.map((coin, index) =>
+      <option key={index} value={coin.quotes.USD.price}>
+        {coin.name} : {coin.quotes.USD.price}
+      </option>)}
+        </select>}
+        <form onSubmit={onSubmit}>
+          <input type="number" placeholder="Insert US Dollar..." />
+          <button>Exchange Coin...</button>
+        </form>
+        <h2>You can get {result} coins!</h2>
     </div>
   );
 }
