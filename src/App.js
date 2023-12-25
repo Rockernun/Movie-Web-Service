652b65c85ae13f6b921abedc0254c950d5b04693
Eokji coding <byunguk98@naver.com>
2023-12-25 20:03:57 +0900

    영화 정보 받아오기
    처음에 로딩 창을 구현한 후, State를 사용해 API로부터 data를 받아서 보여준다. map함수를 사용해서 각각의 movie data에 접근하여 그 값을 변환해 title, summary, genre를 보여준다. 이제 영화 id를 가져와서 movie_details URL을 이용해 앱에서 각각의 영화의 세부 정보 화면으로 넘어가는 것을 구현할 것이다.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
 import {useState, useEffect} from "react";
 
 function App() {
-  const [selectCoins, setSelectCoins] = useState("0");
-  const [result, setResult] = useState("0");
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-const onChange = (event) => {
-  setSelectCoins(event.target.value);
-}
-
-const onSubmit = (event) => {
-  event.preventDefault();
-  const priceUSD = event.target[0].value;
-  calculate(priceUSD);
-}
-
-const calculate = (priceUSD) => {
-  setResult(priceUSD / selectCoins);
-}
-
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+  const json = await (
+    await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
+  ).json();
+  setMovies(json.data.movies);
+  setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers").then(response => 
-    response.json()
-    ).then(json => {
-      setCoins(json);
-      setLoading(false);
-    });
+    getMovies();
   }, []);
-  return (
-    <div>
-      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (<strong>Loading...</strong>) : 
-      <select onChange={onChange}>{coins.map((coin, index) =>
-      <option key={index} value={coin.quotes.USD.price}>
-        {coin.name} : {coin.quotes.USD.price}
-      </option>)}
-        </select>}
-        <form onSubmit={onSubmit}>
-          <input type="number" placeholder="Insert US Dollar..." />
-          <button>Exchange Coin...</button>
-        </form>
-        <h2>You can get {result} coins!</h2>
-    </div>
+  console.log(movies);
+  return ( <div>
+    {loading ? <h1>Loading...</h1> : <div>{movies.map((movie) => 
+      <div key={movie.id}>
+        <img src={movie.medium_cover_image} />
+        <h2>{movie.title}</h2>
+        <p>{movie.summary}</p>
+        <ul>
+          {movie.genres.map((g) => 
+          <li key={g}>{g}</li>
+          )}
+        </ul>
+      </div>
+      )}
+      </div>}
+  </div>
   );
 }
 
