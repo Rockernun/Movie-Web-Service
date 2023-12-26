a5e776f26afbd77d7f09190ed4003f60f13d2692
Eokji coding <byunguk98@naver.com>
2023-12-26 14:22:28 +0900

    영화 세부 정보 Route
    useParams를 통해 URL에 있는 변경되는 값을 반환해서 id 값을 알아낸다.

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,42 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import Details from "../components/Details";
+
 function Detail() {
-  return <h1>Detail</h1>
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovies] = useState({});
+  const {id} = useParams();
+
+  const getMovie = async() => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+    setMovies(json.data.movie);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Details Loading...</h1>
+      ) : (
+        <div>
+          <Details
+            title={movie.title_long}
+            src={movie.medium_cover_image}
+            description={movie.description_full}
+            genres={movie.genres}
+            url={movie.url}
+            rating={movie.rating}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
